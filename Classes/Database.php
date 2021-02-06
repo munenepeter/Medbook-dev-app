8066eac70f667ee93eb0fe15c8a530ac1dabc6f7
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2021-02-06 15:37:31 +0300

    Added a catch block for error handling

--- Classes/Database.php
+++ Classes/Database.php
@@ -6,11 +6,17 @@ Class Database {
     private $dbName = "medbook-app";
 
     public function connect(){
+        try {
         $dsn = 'mysql:host='. $this->host .';dbname='. $this->dbName;
         $pdo = new PDO($dsn, $this->user, $this->password);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $pdo->setAttribute( PDO::ATTR_AUTOCOMMIT, FALSE );
         return $pdo;
+        } catch (PDOException $e) {
+            //throw $error;
+            die('No connection to db' . $e->getMessage());
+        }
+        
     }
 
 }
