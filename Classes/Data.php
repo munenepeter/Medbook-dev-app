4361d055937868c4bf108c3c5306bbe151172144
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2021-02-14 21:55:02 +0300

    Update Data.php
    Still disappointed

--- Classes/Data.php
+++ Classes/Data.php
@@ -12,122 +12,39 @@ class Data extends Database{
         $stmt = $this->connect()->query($sql)->fetchAll();   
         return $stmt; 
     }
-      public function insertData($nameofpatient, $dateofbirth, $gender, $typeofservice, $comments){
-
-      $sql1 =  "INSERT INTO `tbl_patient`(`patient_name`, `patient_dob`, `general_comments`) 
-           VALUES ('$nameofpatient','$dateofbirth','$comments');";
-
-           $this->connect()->query($sql1)->execute();   
-             if($this->connect()->exec($sql1)){
-                echo "success";
-              }else{
-                echo "Failure ";
-              }  
-
-
-            $sql2 = "INSERT INTO `tbl_service`(`patient_id`,`service_type`) 
-            VALUES ((SELECT `patient_id` FROM `tbl_patient` WHERE patient_name = '$nameofpatient'),'$typeofservice');";
-              
-           if($this->connect()->exec($sql2)){
-                echo " success<br>";
-              }else{
-                echo "Failure ";
-              }
- 
-            $sql3 = "INSERT INTO `tbl_gender`(`patient_id`, `gender_type`) 
-            VALUES ((SELECT `patient_id` FROM `tbl_patient` WHERE patient_name = '$nameofpatient'), '$gender');";
-            
-            if($this->connect()->exec($sql3)){
-                echo "success<br>";
-              }else{
-                echo "Failure ";
-              }  
+      public function insertData($nameofpatient, $dateofbirth,$comments){
+        //This is the first query
+        //Inserts the respective rows into the tbl_patient
+        $sql1 = "INSERT INTO `tbl_patient`(`patient_name`, `patient_dob`, `general_comments`) VALUES (?, ?, ?);";
+        $stmt = $this->connect()->prepare($sql1);
+        $stmt->execute([$nameofpatient, $dateofbirth ,$comments]);
          
+        #need to wrap the statements in a dB transaction
+        #so that all the inserts change the database records 
+        #at the same time.
 
-           
-/* 
-            public function insertData(){
- 
-        // insert into respective tables 
-           $sql1 =  "INSERT INTO `tbl_patient`(`patient_name`, `patient_dob`, `general_comments`) 
-           VALUES ('Laptop','1978-09-20 22:04:15','Prepared statements are used to sanitize your input');";
-
-           $this->connect()->query($sql1)->execute();   
-             if($this->connect()->exec($sql1)){
-                echo "success";
-               
-              }else{
-                echo "Failure ";
-              } 
-        
-               $sql2 = "INSERT INTO `tbl_service`(`patient_id`,`service_type`) 
-            VALUES ((SELECT `patient_id` FROM `tbl_patient` WHERE patient_name = 'Laptop'),'inpatient');";
-              
-           if($this->connect()->exec($sql2)){
-                echo " success<br>";
-              }else{
-                echo "Failure ";
-              }
-
-              $sql3 = "INSERT INTO `tbl_gender`(`patient_id`, `gender_type`) 
-            VALUES ((SELECT `patient_id` FROM `tbl_patient` WHERE patient_name = 'Laptop'), 'male');";
-            
-            if($this->connect()->exec($sql3)){
-                echo "success<br>";
-              }else{
-                echo "Failure ";
-              } 
-
-
- */
-
-
-
-
-
-
-
-          /*  $stmt = $sql1->prepare("INSERT INTO `tbl_patient`(`patient_name`, `patient_dob`, `general_comments`) 
-            VALUES (:n, :d, :c);");
-            $stmt->bindParam('n', $nameofpatient);
-            $stmt->bindParam('d', $dateofbirth);
-            $stmt->bindParam('c', $comments);
-              if($stmt->execute()){
-                echo "<br> success";
-              }else{
-                echo "Failure ";
-              }
-           
-           $id = $sql1->lastInsertId();
-          
- 
-          $stmt = $sql1->prepare("INSERT INTO `tbl_gender`( `patient_id`, `gender_id`, `gender_type`) 
-            VALUES (:i, :id, :g);");
-            $stmt->bindParam('i', $id);
-            $stmt->bindParam('id', $id);
-            $stmt->bindParam('g', $gender);
-              if($stmt->execute()){
-                echo "<br> success <br>";
-              }else{
-                echo "Failure ";
-              }
-          
-        
-
-           $stmt = $sql1->prepare("INSERT INTO `tbl_service`( `patient_id`, `service_type`)
-             VALUES (:i, :t);");
-             $stmt->bindParam('i', $id);
-             $stmt->bindParam('t', $typeofservice);
-              if($stmt->execute()){
-                echo "success";
-              }else{
-                echo "Failure ";
-              } */
-         
-     
-       
 
   } 
+    public function insertrestData($nameofpatient, $gender, $typeofservice){
+      //This is the Id of the first query
+        //That is the foreign key that is needed in the other tables
+        $sql0 = "SELECT `patient_id` FROM `tbl_patient` WHERE `patient_name`= ?";
+        $stmt0 = $this->connect()->prepare($sql0);
+        $stmt0->execute([$nameofpatient]);
+        $id = $stmt0->fetch();
+        $id = $id['patient_id'];
 
+        //Second query
+        //Inserts to the tbl_service and takes in the patient_id from the first query
+        $sql2 = "INSERT INTO `tbl_service`(`patient_id`,`service_type`) VALUES(?,?);";
+        $stmt2 = $this->connect()->prepare($sql2);
+        $stmt2->execute ([$id, $typeofservice]);
+
+
+        //Third query
+        $sql3 = "INSERT INTO `tbl_gender`(`patient_id`, `gender_type`)  VALUES(?,?);";
+        $stmt3 = $this->connect()->prepare($sql3);
+        $stmt3->execute([$id, $gender]);
+    }
 }
 //error still cannot upload could not upload ... geuss im not getting the <job class=""></job>
