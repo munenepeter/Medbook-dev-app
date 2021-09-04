b2a451becad785e14a70d7e5879cba96b2d96055
Peter Munene <peternjega7@gmail.com>
2021-09-04 19:30:26 +0300

    Typos

--- Classes/Database.php
+++ Classes/Database.php
@@ -9,12 +9,12 @@ Class Database {
     public function connect(){
         try {
         $dsn = 'mysql:host='. $this->host .';dbname='. $this->dbName;
-        $pdo = new PDO($dsn, $this->user, $this->password);
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        $pdo->setAttribute( PDO::ATTR_AUTOCOMMIT, FALSE );
+        $pdo = new \PDO($dsn, $this->user, $this->password);
+        $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        $pdo->setAttribute( \PDO::ATTR_AUTOCOMMIT, FALSE );
         $this->pdo = $pdo;
         return $pdo;
-        } catch (PDOException $e) {
+        } catch (\PDOException $e) {
             //throw $error;
             die('No connection to db' . $e->getMessage());
         }
